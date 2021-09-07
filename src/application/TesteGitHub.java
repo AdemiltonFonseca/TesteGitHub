942ea5088631deff762189fa21dfbca3ed0b1a95
Ademilton Fonseca <ademiltonfonseca@gmail.com>
2021-09-07 12:07:23 -0300

    Projeto criado

---
+++ src/application/TesteGitHub.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteGitHub {
+    
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Hello World com Git Hub.");
+        
+        
+        input.close();
+    } // Fim do método main    
+} // Fim da classe TesteGitHub
